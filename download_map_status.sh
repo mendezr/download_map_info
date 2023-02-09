a9e896b4c93cf7632ea4f47405595596e655a5f3
Raul Mendez <raul.mendez@isglobal.org>
2023-02-09 15:34:13 +0100

    Add comments

--- download_map_status.sh
+++ download_map_status.sh
@@ -6,24 +6,24 @@
 #Email        : raul.mendez@isglobal.org
 #Description  : Download google sheet from isglobal drive
 #Creation Date: 09-02-2023
-#Last Modified: Thu 09 Feb 2023 12:46:40 PM CET
+#Last Modified: Thu 09 Feb 2023 03:33:34 PM CET
 ###################################################################
 
 # Load the local rclone
 # I need to download a newer version of rclone to use the google api
 export PATH=$PATH:$HOME/bin/
 
-# Check if rclone exits
+# Check if rclone exits if not exit with error code
 if ! command -v rclone &> /dev/null
 then
     echo "<rclone> could not found"
     exit 1
 fi
 
+# Folder when the file will be downloaded
 FOLDER=/home/isglobal.lan/rmendez/test_rclone/maps
+# Full path of the file
 FILE=$FOLDER/"Dataset Periodic Updates.xlsx"
-
-echo $FILE
 
 if [[ -f "${FILE}" ]];
 then
@@ -39,6 +39,8 @@ rclone copy isglobal:"/maps/Dataset Periodic Updates.xlsx" .
 
 if [[ ! -f "${FILE}" ]];
 then
+    # Check if the file is download, if not restore it using
+    # the backup
     echo Fail, restore the backup
     mv $FOLDER/.data_update.xlsx $FILE 
     exit 1
@@ -46,7 +48,7 @@ fi
 
 if [[ -f ".data_update.xlsx" ]];
 then
-    # Check if file exist previously, rename it as backup
+    # If the map_info was download, then delete the backup file
     rm .data_update.xlsx
 fi
 
