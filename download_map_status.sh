6203601bf5db22bdd8de387746f489b5eb373817
Raul Mendez <raul.mendez@isglobal.org>
2023-02-10 13:28:09 +0100

    Replace space with underscore and create backup

--- download_map_status.sh
+++ download_map_status.sh
@@ -6,11 +6,12 @@
 #Email        : raul.mendez@isglobal.org
 #Description  : Download google sheet from isglobal drive
 #Creation Date: 09-02-2023
-#Last Modified: Thu 09 Feb 2023 03:33:34 PM CET
+#Last Modified: Fri 10 Feb 2023 01:28:02 PM CET
 ###################################################################
 
 # Load the local rclone
 # I need to download a newer version of rclone to use the google api
+# I have the binary in my home directory
 export PATH=$PATH:$HOME/bin/
 
 # Check if rclone exits if not exit with error code
@@ -21,35 +22,39 @@ then
 fi
 
 # Folder when the file will be downloaded
-FOLDER=/home/isglobal.lan/rmendez/test_rclone/maps
+FOLDER=/PROJECTES/ADAPTATION/core/code/health/download/
 # Full path of the file
-FILE=$FOLDER/"Dataset Periodic Updates.xlsx"
+FILE=$FOLDER/"Dataset_Periodic_Updates.xlsx"
+ 
+cd $FOLDER || exit
 
 if [[ -f "${FILE}" ]];
 then
     # Check if file exist previously, rename it as backup
-    echo File found, remove it
-    mv $FILE $FOLDER/.data_update.xlsx
+    echo File found, create the backup
+    mv "${FILE}" data_update.xlsx
 fi
 
-cd $FOLDER
 echo "Download the map file"
 
 rclone copy isglobal:"/maps/Dataset Periodic Updates.xlsx" .
+find . -name "Dataset Periodic Updates.xlsx" -type f | while read -r file; do mv "$file" "$(echo "$file" | tr ' ' _)"; done
+
 
 if [[ ! -f "${FILE}" ]];
 then
     # Check if the file is download, if not restore it using
     # the backup
     echo Fail, restore the backup
-    mv $FOLDER/.data_update.xlsx $FILE 
+    mv $FOLDER/data_update.xlsx $FILE 
     exit 1
 fi
 
-if [[ -f ".data_update.xlsx" ]];
+if [[ -f "data_update.xlsx" ]];
 then
     # If the map_info was download, then delete the backup file
-    rm .data_update.xlsx
+    echo Removing the backup
+    rm data_update.xlsx
 fi
 
 echo Done
