c744a46c1f0f018e3c5ad894d37ddbb92221b17e
Raul Mendez <raul.mendez@isglobal.org>
2023-04-13 13:08:11 +0200

    Add more docstring

--- dowload_map_info.sh
+++ dowload_map_info.sh
@@ -6,7 +6,7 @@
 #Email        : raul.mendez@isglobal.org
 #Description  : Download google sheet from isglobal drive
 #Creation Date: 09-02-2023
-#Last Modified: Thu 13 Apr 2023 01:02:24 PM CEST
+#Last Modified: Thu 13 Apr 2023 01:07:58 PM CEST
 ###################################################################
 
 # Load the local rclone
@@ -38,6 +38,8 @@ fi
 
 echo "Download the map file"
 
+# copy the Population_NumberRegions.xlsx from the drive, I configure the server
+# as `isglobal`
 rclone copy isglobal:"/maps/$FILENAME" .
 
 if [[ ! -f "${FILE}" ]];
